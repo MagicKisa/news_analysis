33ea991bdad25849cad0d3bd3c6cf00f950fabb6
Maga <maga@LAPTOP-1I6JJP9Q>
2024-03-02 19:20:17 +0300

    deleted bot_token, add config_reader

--- bot/main.py
+++ bot/main.py
@@ -6,15 +6,13 @@ from aiogram.enums import ParseMode
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.filters import Command, StateFilter
 from aiogram.utils.keyboard import InlineKeyboardBuilder
-
+from config_reader import config
 from handlers import about_work, common, classification_news
 
 # Устанавливаем уровень логгирования
 logging.basicConfig(level=logging.INFO)
 
-# Замените 'YOUR_BOT_TOKEN' на токен вашего бота
-TOKEN = '6525731519:AAHfDAZHDQiJCb3Fl0CMB3v2YYtG5jaFtxc'
-bot = Bot(token=TOKEN)
+bot = Bot(token=config.bot_token.get_secret_value())
 dp = Dispatcher()
 
 
